32ea860c7311d10d69ee508bb560272e3cf2f2f4
Yoann CHEVESSIER <hello@yoannchevessier.fr>
2020-01-25 09:47:12 +0100

    remove: composition class, refactor: pizza livraison class, create: ingredient JDBC class

---
+++ project/src/project/Ingredient.java
@@ -0,0 +1,37 @@
+package project;
+
+public class Ingredient {
+
+	private int id;
+	private String nom;
+	private String provenance;
+	
+	public Ingredient(int id, String nom, String provenance) {
+		this.id = id;
+		this.nom = nom;
+		this.provenance = provenance;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getProvenance() {
+		return provenance;
+	}
+	public void setProvenance(String provenance) {
+		this.provenance = provenance;
+	}
+	
+	
+}
--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Pizza pizza1 = new Pizza(1, "Imagine", 5.90);
+		// pizza1.setIngredient("test");
+		// String p = pizza1.getNom();
+		// System.out.println("Voici la pizza à afficher: " + p);
+		//
+		// System.out.println(pizza1.getIngredients());
 	}
 
 }
