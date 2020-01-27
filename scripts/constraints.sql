32023663474f1a028965a87771b981b82bfb6071
Yoann CHEVESSIER <hello@yoannchevessier.fr>
2020-01-27 15:05:24 +0100

    bug constraints majuscules table names

--- scripts/constraints.sql
+++ scripts/constraints.sql
@@ -1,6 +1,6 @@
-ALTER TABLE client ADD CONSTRAINT chk_client CHECK (solde >= 0);
-ALTER TABLE ingredient ADD CONSTRAINT uni_ingredien UNIQUE (nom);
+ALTER TABLE Client ADD CONSTRAINT chk_client CHECK (solde >= 0);
+ALTER TABLE Ingredient ADD CONSTRAINT uni_ingredien UNIQUE (nom);
 -- ALTER TABLE livraison ADD CONSTRAINT chk_livraison TIMEDIFF(date_livraison, date_commande) > "00:00";
-ALTER TABLE pizza ADD CONSTRAINT uni_pizza UNIQUE (nom);
-ALTER TABLE pizza ADD CONSTRAINT chk_pizza CHECK (prix_base > 0);
-ALTER TABLE taille ADD CONSTRAINT chk_taille CHECK (ratio > 0);
+ALTER TABLE Pizza ADD CONSTRAINT uni_pizza UNIQUE (nom);
+ALTER TABLE Pizza ADD CONSTRAINT chk_pizza CHECK (prix_base > 0);
+ALTER TABLE Taille ADD CONSTRAINT chk_taille CHECK (ratio > 0);
