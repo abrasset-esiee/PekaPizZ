4e06ad12f759b4360e129f9c9a1335b3fcc42312
abrasset-esiee <59836181+abrasset-esiee@users.noreply.github.com>
2020-01-28 11:48:33 +0100

    Update constraints.sql

--- scripts/constraints.sql
+++ scripts/constraints.sql
@@ -1,7 +1,7 @@
 USE Pekapizz;
 ALTER TABLE Client ADD CONSTRAINT chk_client CHECK (solde >= 0);
 ALTER TABLE Ingredient ADD CONSTRAINT uni_ingredien UNIQUE (nom);
--- ALTER TABLE livraison ADD CONSTRAINT chk_livraison TIMEDIFF(date_livraison, date_commande) > "00:00";
+ALTER TABLE Livraison ADD CONSTRAINT chk_livraison CHECK (TIMEDIFF(date_livraison, date_commande) > "00:00:00");
 ALTER TABLE Pizza ADD CONSTRAINT uni_pizza UNIQUE (nom);
 ALTER TABLE Pizza ADD CONSTRAINT chk_pizza CHECK (prix_base > 0);
 ALTER TABLE Taille ADD CONSTRAINT chk_taille CHECK (ratio > 0);
